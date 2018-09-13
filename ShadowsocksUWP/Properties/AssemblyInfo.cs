e25cb908b2dc80c7c848b6f09a4684a6207215d2
MicroOwl <bestowlcity@outlook.com>
2018-09-13 20:42:24 +0800

    Rename app project to ShadowsocksUWP and update package manifest

--- ShadowsocksUWP/Properties/AssemblyInfo.cs
+++ ShadowsocksUWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("shadowsocks-uwp")]
+[assembly: AssemblyTitle("Shadowsocks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("shadowsocks-uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Shadowsocks")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

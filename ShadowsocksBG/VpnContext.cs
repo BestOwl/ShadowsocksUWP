0aa1be950f14d0afcc2524e461640836715e7269
NightOwl <midnightow1@outlook.com>
2018-10-08 21:19:56 +0800

    Exclude ss-remote ip from routing list

--- ShadowsocksBG/VpnContext.cs
+++ ShadowsocksBG/VpnContext.cs
@@ -21,11 +21,14 @@ namespace ShadowsocksBG
             ExcludeLocalSubnets = true
         };
 
+        internal VpnTrafficFilterAssignment trafficScope = new VpnTrafficFilterAssignment();
+
         internal bool isInited { get; private set; } = false;
 
         public void Init()
         {
             var inclusionRoutes = routeScope.Ipv4InclusionRoutes;
+            CreateRouteList(inclusionRoutes, new HostName("188.166.248.242"));
             // qzworld.net
             //inclusionRoutes.Add(new VpnRoute(new HostName("188.166.248.242"), 32));
             // DNS server
@@ -44,6 +47,16 @@ namespace ShadowsocksBG
             isInited = true;
         }
 
+        /// <summary>
+        /// Create route list to route all traffic except ss-remote to VPN interface
+        /// </summary>
+        /// <param name="list">Route inclusion list</param>
+        /// <param name="dst">ss-remote ip address</param>
+        /// <remarks>
+        /// This is the temporary solution to route all traffic to the VPN interface since ExclusionRoutes dose not work at all
+        /// and we cant get any support from microsoft.
+        /// </remarks>
+        /// <seealso cref="https://social.msdn.microsoft.com/Forums/en-US/eddf881d-d823-4a69-8aa3-df661f5abc0b/routing-issue-with-uwp-vpnplugin?forum=wpdevelop"/>
         private void CreateRouteList(IList<VpnRoute> list, HostName dst)
         {
             int[] dstIP = new int[4];
@@ -67,8 +80,32 @@ namespace ShadowsocksBG
                 {
                     continue;
                 }
-                else if (i == dstIP[1])
+                else if (i == dstIP[0])
                 {
+                    for (int i2 = 0; i2 <= 255; i2++)
+                    {
+                        if (i2 == dstIP[1])
+                        {
+                            for (int i3 = 0; i3 <= 255; i3++)
+                            {
+                                if (i3 == dstIP[2])
+                                {
+                                    for (int i4 = 1; i4 < 255; i4++)
+                                    {
+                                        if (i4 == dstIP[3])
+                                        {
+                                            continue;
+                                        }
+                                        list.Add(new VpnRoute(new HostName(string.Format("{0}.{1}.{2}.{3}", i, i2, i3, i4)), 32));
+                                    }
+                                    continue;
+                                }
+                                list.Add(new VpnRoute(new HostName(string.Format("{0}.{1}.{2}.0", i, i2, i3)), 24));
+                            }
+                            continue;
+                        }
+                        list.Add(new VpnRoute(new HostName(string.Format("{0}.{1}.0.0", i, i2)), 16));
+                    }
                     continue;
                 }
 
